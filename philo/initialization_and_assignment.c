2064554fe7b619abb06410cf33baa63174b1dc3f
Welepy-dev <mwsilva.contato@gmail.com>
2024-12-10 22:14:24 +0000

    philosopher initialization incomplete [missing fork assignment logic], also add "data" to the philosopher struct

--- philo/initialization_and_assignment.c
+++ philo/initialization_and_assignment.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   initialization_and_assignment.c                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marcsilv <marcsilv@student.42.fr>          +#+  +:+       +#+        */
+/*   By: welepy <welepy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/26 16:19:09 by marcsilv          #+#    #+#             */
-/*   Updated: 2024/10/26 16:53:12 by marcsilv         ###   ########.fr       */
+/*   Updated: 2024/12/10 22:10:52 by welepy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,26 +17,45 @@
 	int	i;
 
 	i = 0;
-	data->forks = malloc(sizeof(t_fork) * data->nb_philos);
-	data->philos = malloc(sizeof(t_philo) * data->nb_philos);
+	data->forks = malloc(sizeof(t_fork) * data->number_of_philos);
+	data->philosopher = malloc(sizeof(t_philo) * data->number_of_philos);
 	pthread_mutex_init(&data->print_mutex, NULL);
 	pthread_mutex_init(&data->death_mutex, NULL);
-	while (i < data->nb_philos)
+	while (i < data->number_of_philos)
 	{
 		data->forks[i].fork_id = i;
 		pthread_mutex_init(&data->forks[i].fork, NULL);
-		data->philos[i].id = i;
-		data->philos[i].meals = 0;
-		data->philos[i].is_full = false;
-		data->philos[i].last_eat = 0;
-		data->philos[i].data = data;
+		data->philosopher[i].id = i;
+		data->philosopher[i].meals = 0;
+		data->philosopher[i].is_full = false;
+		data->philosopher[i].last_meal = 0;
+		data->philosopher[i].data = data;
 		i++;
 	}
 	gettimeofday(&data->start, NULL);
 }*/
 
+static void	fork_assignment(t_philo *philosopher, t_fork *fork, int pos)
+{
+	philosopher->right_fork = &fork[pos];
+	philosopher->left_fork = &fork[(pos + 1) % philosopher->data->number_of_philos];
+}
+
 void	initialization_and_assignment(t_data *data)
 {
-	data->forks = h_malloc(sizeof(t_fork) * data->nb_philos);
-	data->philos = h_malloc(sizeof(t_philo) * data->nb_philos);
+	int	i;
+
+	i = -1;
+	data->forks = h_malloc(sizeof(t_fork) * data->number_of_philos);
+	data->philosopher = h_malloc(sizeof(t_philo) * data->number_of_philos);
 	data->end = false;
+	while (++i < data->number_of_philos)
+	{
+		mutex_handler(&data->forks[i].fork, INIT);
+		data->forks[i].fork_id = i;
+		data->philosopher[i].id = i + 1;
+		data->philosopher[i].is_full = false;
+		data->philosopher[i].meals = 0;
+		data->philosopher[i].last_meal = 0;
+	}
+}
